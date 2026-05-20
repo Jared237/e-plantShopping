37277518f1467cf06a2f609b2da59712199a6aa6
Jared237 <ngahaneyonkam.jaredlael@ictuniversity.edu.cm>
2026-05-21 00:08:43 +0200

    Refactor store configuration and imports
    Updated import path for CartSlice and added CSS import.

--- src/store.js
+++ src/store.js
@@ -1,8 +1,24 @@
+// src/index.js or src/store.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice';
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+import './index.css';
+import App from './App';
+import cartReducer from './components/CartSlice';
+
+// Configure the store with cart reducer
+const store = configureStore({
+  reducer: {
+    cart: cartReducer, // Export the reducer as default to use in store
+  },
 });
-export default store
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
